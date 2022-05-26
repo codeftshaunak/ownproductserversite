a0d63f947351e580a4fa8ec3b4bf0af0f5a815c6
codeftshaunak <sagardeyyash19@gmail.com>
2022-05-26 08:49:03 +0600

    install dependancy

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const {
         MongoClient,
-        ServerApiVersion
+        ServerApiVersion,
+        ObjectId
 } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Middleware
 app.use(cors());
 app.use(express.json());
 
+//Mongodb Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f3nx9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
         useNewUrlParser: true,
@@ -28,6 +31,15 @@ async function run() {
                         const cursor = productCollection.find(query);
                         const products = await cursor.toArray();
                         res.send(products);
+                })
+
+                app.get('/productsss/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = {
+                                _id: ObjectId(id)
+                        }
+                        const product = await productCollection.findOne(query);
+                        res.send(product);
                 })
 
         } finally {}
